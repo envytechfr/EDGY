032d4af0c34758ef533bf354f28c9ed943ee9d82
envytechfr <robin@envytech.fr>
2024-07-29 15:08:32 +0200

    Create Login.js

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Login({ setIsAuthenticated }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    axios.post('/api/login', { username, password })
+      .then(response => {
+        setIsAuthenticated(true);
+      })
+      .catch(() => {
+        alert('Login failed');
+      });
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
